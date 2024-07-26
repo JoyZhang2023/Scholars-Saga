464840ca09a5c49bc02710cbb4d8afab0fab870e
JoyZhang2023 <jocelyn-zhang@live.com>
2024-07-25 20:04:48 -0700

    add nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>app/page.tsx</code>
+        </p>
+        <Link href="counselor-dashboard">Counselor Dashboard</Link>
+      </div>
+    </main>
+  );
 'use client'
 
 import { useRouter } from 'next/navigation';
