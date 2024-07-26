412d63a66feb54d8189099dd44650e026e2f9c44
JoyZhang2023 <jocelyn-zhang@live.com>
2024-07-25 19:35:26 -0700

    add dashboard

---
+++ app/counselro-dashboard/page.tsx
@@ -0,0 +1,13 @@
+import styles from "@/app/page.module.css";
+
+export default function Counselor() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <header>
+                    Counselor Dashboard
+                </header>
+            </div>
+        </main>
+    );
+}
