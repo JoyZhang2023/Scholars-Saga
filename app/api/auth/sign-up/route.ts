4dd7dc20a0581c73e641c5888a6f5f63bd2aa4a0
JoyZhang2023 <jocelyn-zhang@live.com>
2024-08-19 16:25:16 -0700

    update

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -6,9 +6,9 @@ export async function POST(request: Request) {
     try {
         const {email, password} = await request.json();
         // validate email and password, maybe zod
-        
-        const hashedpassword = await hash(password, 10);
 
+        const hashedpassword = await hash(password, 10);
+        
         // sql insert user into db
         // const response = await sql` 
         //     INSERT INTO users (email, password)
