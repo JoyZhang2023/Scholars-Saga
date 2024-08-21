43dfcc811b4b6ba9a2e7a6a895c13f584c423664
JoyZhang2023 <jocelyn-zhang@live.com>
2024-08-20 18:27:32 -0700

    database test

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -1,18 +1,19 @@
 import { NextResponse } from "next/server";
 import { hash } from 'bcrypt';
+import prisma from "@/lib/prisma";
 
 export async function POST(request: Request) {
 
     try {
-        const {email, password} = await request.json();
+        const {email, password, role} = await request.json();
         // validate email and password, maybe zod
 
         const hashedpassword = await hash(password, 10);
         
         // sql insert user into db
         // const response = await sql` 
-        //     INSERT INTO users (email, password)
-        //     VALUES (${email}, ${hashedpassword})`;
+        //     INSERT INTO users (email, password, role)
+        //     VALUES (${email}, ${hashedpassword}, ${role})`;
         
     } catch (e) {
         console.log({ e });
