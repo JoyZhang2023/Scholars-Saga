f4f4d1cb04724c8e6966b3af1c46312157498e8c
NW Miller <36825393+nwm516@users.noreply.github.com>
2024-09-01 23:36:53 -0700

    scholar's-saga_3_4_login_creation_session_mgmt (#34)
    * Database setup
    
    Establish pieces for basic database set up with Prisma
    
    * Create login page and session management
    
    This creates a basic login page and sets the groundwork for session management throughout the application.
    
    ---------
    
    Co-authored-by: JoyZhang2023

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,28 +1,61 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import styles from './styles/ProfileCard.module.css';
+import { Box, Button, Avatar, Typography } from '@mui/material';
+import { useAuth } from '../context/authContext';
 
 const ProfileCard: React.FC = () => {
+  const { user, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <div className={styles.profileCard}>
-      <div className={styles.profilePicture}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: '8px',
+        boxShadow: 3,
+        p: 2,
+        width: '200px',
+        textAlign: 'center',
+      }}
+    >
+      <Avatar sx={{ width: 100, height: 100, mb: 2 }}>
         <Image
-          src="/generic_profile_pic.png"
+          src={user && user.profile_picture ? user.profile_picture : "/images/generic_profile_pic.png"}
           alt="Profile Picture"
           width={100}
           height={100}
+          objectFit="cover"
         />
-      </div>
-      <div className={styles.profileButtons}>
-        <Link href="/login" className={styles.profileButton}>
-          Login
-        </Link>
-        <Link href="/signup" className={`${styles.profileButton} ${styles.newAccountButton}`}>
-          New Account
-        </Link>
-      </div>
-    </div>
+      </Avatar>
+      <Typography variant="h6" gutterBottom>
+        {user ? `Welcome, ${user.first_name}!` : 'Welcome!'}
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        component={Link}
+        href={user ? "/profile" : "/login"}
+      >
+        {user ? "View Profile" : "Login"}
+      </Button>
+      {user && (
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={handleLogout}
+          sx={{ mt: 1 }}
+        >
+          Logout
+        </Button>
+      )}
+    </Box>
   );
 };
 
---
+++ prisma/migrations/20240819030221_add_password_field_students_counselors/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "counselors" ADD COLUMN     "password" VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "students" ADD COLUMN     "password" VARCHAR(255);
