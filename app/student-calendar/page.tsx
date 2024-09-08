46fe7036627975a4097b834cae13901a225d75e2
JoyZhang2023 <jocelyn-zhang@live.com>
2024-09-07 22:01:04 -0700

    clean up

--- app/student-calendar/page.tsx
+++ app/student-calendar/page.tsx
@@ -2,36 +2,28 @@
 import NavbarStudentPrototype from '@/components/Navbar_student';
 import React, { FC } from 'react';
 import {Box} from "@mui/system";
-<<<<<<< HEAD
 import Register_Component from "@/components/Register_Component";
 import Educational_Plan from "@/components/Educational_Plan_Creator";
 import Educational_Plan_Creator from "@/components/Educational_Plan_Creator";
 import EduPlanCreator from "@/components/Edu_Plan";
-=======
 import Theme_Component from "@/components/Theme_Component";
->>>>>>> main
+import { CssBaseline } from '@mui/material';
 
 
 
 
 const StudentCalendar = () => {
     return (
-<<<<<<< HEAD
-        <ThemeProvider theme={themeOptions}>
+        <div>
             <CssBaseline />
             <Box sx={{ display: 'flex', width: '100%', height: '100vh' }}>
                 <NavbarStudentPrototype />
                 <Box sx={{ flexGrow: 1, p: 3, mt: 6 }}>
-                    <EduPlanCreator/>
+                    <EduPlanCreator />
+                    <Theme_Component />
                 </Box>
-=======
-        <Box sx={{ display: 'flex', width: '100%', height: '100vh' }}>
-            <NavbarStudentPrototype />
-            <Box sx={{ flexGrow: 1, p: 3, mt: 5 }}>
-                <Theme_Component />
->>>>>>> main
-            </Box>
-        </Box>
+            </Box>    
+        </div>
     );
 };
 
