9e6df19b119d5dc2b02dbf757ade92f1fc60d9c8
JoyZhang2023 <jocelyn-zhang@live.com>
2024-09-13 15:13:54 -0700

    fix authOptions import link

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import SignInForm from './form';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { Box, Container, Typography, TextField, Button } from '@mui/material';
 import styles from './signinform.module.css';
 
